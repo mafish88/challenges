b853d548e53365ef74357b9e935252dc1d863445
Michael Fisher <mafish88@icloud.com>
2023-10-09 16:56:23 -0400

    multiply

---
+++ index.js
@@ -0,0 +1,18 @@
+function hello(name){
+   const message = 'hello '
+   return message + name
+}
+
+
+
+
+
+
+
+
+function multiply(num1, num2){
+    const product = num1 * num2
+    return num1 + ' X ' + num2  +  ' = ' + product
+}
+
+console.log(multiply(2,3))
